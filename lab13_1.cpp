ab653d8e573115709cc8aff837be8fee2c8fb605
Namphet <namphetnanaphet@gmail.com>
2026-01-09 21:19:23 +0700

    lab13_1.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,28 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double a[], int n, double b[]) {
+    double sum = 0;
+    double sumSq = 0;
+    double productLog = 0;
+    double sumHarmonic = 0;
+    double maxVal = a[0];
+    double minVal = a[0];
+
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+        sumSq += a[i] * a[i];
+        productLog += log(a[i]);
+        sumHarmonic += 1.0 / a[i];
+        if (a[i] > maxVal) maxVal = a[i];
+        if (a[i] < minVal) minVal = a[i];
+    }
+
+    double mean = sum / n;
+    b[0] = mean;
+    b[1] = sqrt((sumSq / n) - (mean * mean));
+    b[2] = exp(productLog / n);
+    b[3] = n / sumHarmonic;
+    b[4] = maxVal;
+    b[5] = minVal;
+}
